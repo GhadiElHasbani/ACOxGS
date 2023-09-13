34be1ff1e36f3e2b56d4540eaedd9431cec1f041
Ghadi El Hasbani <ghadi.elhasbani@lau.edu>
2023-09-13 19:05:59 +0300

    Update packages_download.R

--- packages_download.R
+++ packages_download.R
@@ -2,31 +2,27 @@ options(timeout = 10000000000000000000000000)
 
 if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
-BiocManager::install("curatedBladderData")
-BiocManager::install("curatedOvarianData")
 BiocManager::install("GSAR")
+BiocManager::install("GSEABenchmarkeR")
 BiocManager::install("KEGGgraph")
 BiocManager::install("CHRONOS")
 BiocManager::install("graphite")
-BiocManager::install("DOSE")
 BiocManager::install("limma")
 BiocManager::install("org.Hs.eg.db")
+BiocManager::install("annotate")
 
-require(mND)
 require(dplyr)
 require(ggplot2)
 require(purrr)
 require(pcaPP) # GS dependency
 require(igraph) # GS dependency
-require(limma) # GS dependency
-require(curatedBladderData) # GS data
-require(curatedOvarianData) # GS data
+require(limma) # DEA tool / GS dependency
+require(LEANR) # LEAN package
+require(doMC) # LEAN dependency
 require(GSAR) # p53 data
-require(DOSE) # Enrichment tool
 # For KEGG network
 require(org.Hs.eg.db)
 require(KEGGgraph)
 require(XML)
 require(CHRONOS)
 require(graphite)
-
