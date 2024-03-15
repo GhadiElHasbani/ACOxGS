39c004ad4d4a4cb7f8bb4637d3d10528acc44acd
Ghadi El Hasbani <ghadielhasbani@gmail.com>
2024-03-15 21:17:28 +0100

    improved code format
    added guide

--- roc.R
+++ roc.R
@@ -9,11 +9,7 @@ library(GSAR) # p53 data
 library(GSEABenchmarkeR) # GSEABenchmark data
 library(org.Hs.eg.db)
 
-run <- 3 # can be any of 1 to 10 (int)
-dataset <- '20291' #can be any of 20291, 5281VCX, 8762
-disease <- "HD" #can be any of "PD", "AD", "HD" in this order corresponding to dataset ID above
-
-generate_ROC <- function(run, dataset, disease){
+generate_ROC <- function(run, dataset, disease, variable_cutoff = FALSE){
     aco <- read.csv(paste("Enrichment/Run ", run, "/", dataset, "/aco_gamma_40_enrichment_", dataset, "_", run,".csv", sep = "")) %>% arrange(pathway.name)
     gs <- read.csv(paste("Enrichment/Run ", run, "/", dataset, "/gs_enrichment_", dataset, "_", run, ".csv", sep = "")) %>% arrange(pathway.name)
     lean <- read.csv(paste("Enrichment/Run ", run, "/", dataset, "/lean_enrichment_", dataset, "_", run, ".csv", sep = "")) %>% arrange(pathway.name)
@@ -73,57 +69,90 @@ generate_ROC <- function(run, dataset, disease){
     #resulting NA values have same index
     true_y <- true_y[!is.na(aco[,'adj_p.value'])]
     
-    
-    aco_y <- get_y(aco[,'adj_p.value'][!is.na(aco[,'adj_p.value'])])
-    gs_y <- get_y(gs[,'adj_p.value'][!is.na(gs[,'adj_p.value'])])
-    lean_y <- get_y(lean[,'adj_p.value'][!is.na(lean[,'adj_p.value'])])
-    limma_y <- get_y(limma[,'adj_p.value'][!is.na(limma[,'adj_p.value'])])
-    
-    
-    #plot_ROC <- function(y, true_y){
-    #  return(roc(true_y,y,
-    #                  smoothed = TRUE,
-    #                  # arguments for plot
-    #                  plot=TRUE, auc.polygon=TRUE, max.auc.polygon=TRUE, grid=TRUE,
-    #                  print.auc=TRUE))
-    #}
-    
-    #aco_roc <- plot_ROC(aco_y, true_y)
-    #gs_roc <- plot_ROC(gs_y, true_y)
-    #lean_roc <- plot_ROC(lean_y, true_y)
-    #limma_roc <- plot_ROC(limma_y, true_y)
-    aco_roc <- roc(aco_y, true_y, direction = '>')
-    gs_roc <- roc(gs_y, true_y)
-    lean_roc <- roc(lean_y, true_y)
-    limma_roc <- roc(limma_y, true_y)
-    
-    ggroc(list(ACO = aco_roc, GS = gs_roc, LEAN = lean_roc, limma = limma_roc))
-    
-    #### for variable cutoffs
-    
-    library(ROCR)
-    
-    aco_y <- aco[,'adj_p.value'][!is.na(aco[,'adj_p.value'])]
-    gs_y <- gs[,'adj_p.value'][!is.na(gs[,'adj_p.value'])]
-    lean_y <- lean[,'adj_p.value'][!is.na(lean[,'adj_p.value'])]
-    limma_y <- limma[,'adj_p.value'][!is.na(limma[,'adj_p.value'])]
-    
-    
-    aco_roc <- performance(prediction(aco_y, true_y),"tpr","fpr")
-    gs_roc <- performance(prediction(gs_y, true_y),"tpr","fpr")
-    lean_roc <- performance(prediction(lean_y, true_y),"tpr","fpr")
-    limma_roc <- performance(prediction(limma_y, true_y),"tpr","fpr")
-    
-    plot(aco_roc)
-    abline(a = 0, b = 1)
-    
-    plot(gs_roc)
-    abline(a = 0, b = 1)
-    
-    plot(lean_roc)
-    abline(a = 0, b = 1)
-    
-    plot(limma_roc)
-    abline(a = 0, b = 1)
+    if(!variable_cutoff){ #also makes possible to combine all in one plot
+        aco_y <- get_y(aco[,'adj_p.value'][!is.na(aco[,'adj_p.value'])])
+        gs_y <- get_y(gs[,'adj_p.value'][!is.na(gs[,'adj_p.value'])])
+        lean_y <- get_y(lean[,'adj_p.value'][!is.na(lean[,'adj_p.value'])])
+        limma_y <- get_y(limma[,'adj_p.value'][!is.na(limma[,'adj_p.value'])])
+        
+        
+        #plot_ROC <- function(y, true_y){
+        #  return(roc(true_y,y,
+        #                  smoothed = TRUE,
+        #                  # arguments for plot
+        #                  plot=TRUE, auc.polygon=TRUE, max.auc.polygon=TRUE, grid=TRUE,
+        #                  print.auc=TRUE))
+        #}
+        
+        #aco_roc <- plot_ROC(aco_y, true_y)
+        #gs_roc <- plot_ROC(gs_y, true_y)
+        #lean_roc <- plot_ROC(lean_y, true_y)
+        #limma_roc <- plot_ROC(limma_y, true_y)
+        aco_roc <- roc(aco_y, true_y)
+        gs_roc <- roc(gs_y, true_y)
+        lean_roc <- roc(lean_y, true_y)
+        tryCatch(
+            {
+                limma_roc <- roc(limma_y, true_y)
+                
+                ggroc(list(ACO = aco_roc, GS = gs_roc, LEAN = lean_roc, limma = limma_roc))
+            },
+            error = function(cond) {
+                message("limma produced no results for this experiment with this dataset. Excluding...")
+                
+                ggroc(list(ACO = aco_roc, GS = gs_roc, LEAN = lean_roc))
+            }
+        )
+        
+        
+    }else{
+        #### for variable cutoffs
+        
+        library(ROCR)
+        
+        aco_y <- aco[,'adj_p.value'][!is.na(aco[,'adj_p.value'])]
+        gs_y <- gs[,'adj_p.value'][!is.na(gs[,'adj_p.value'])]
+        lean_y <- lean[,'adj_p.value'][!is.na(lean[,'adj_p.value'])]
+        limma_y <- limma[,'adj_p.value'][!is.na(limma[,'adj_p.value'])]
+        
+        
+        aco_roc <- performance(prediction(aco_y, true_y),"tpr","fpr")
+        gs_roc <- performance(prediction(gs_y, true_y),"tpr","fpr")
+        lean_roc <- performance(prediction(lean_y, true_y),"tpr","fpr")
+        
+        plot(aco_roc)
+        abline(a = 0, b = 1)
+        
+        plot(gs_roc)
+        abline(a = 0, b = 1)
+        
+        plot(lean_roc)
+        abline(a = 0, b = 1)
+        
+        tryCatch(
+            {
+                limma_roc <- performance(prediction(limma_y, true_y),"tpr","fpr")
+                
+                plot(limma_roc)
+                abline(a = 0, b = 1)
+            },
+            error = function(cond) {
+                message("limma produced no results for this experiment with this dataset. Excluding...")
+            }
+        )
+        
+        
+    }
     
 }
+
+#Example 
+
+#make sure to set the directory to be the cloned repository. something like: "~/Desktop/ACOxGS" with appropriate windows format
+setwd(.......)
+
+run <- 3 # can be any of 1 to 10 (int)
+dataset <- '8762' #can be any of 20291, 5281VCX, 8762 (need to be matched to appropriate disease argument)
+disease <- "HD" #can be any of "PD", "AD", "HD" in this order corresponding to dataset ID above
+
+generate_ROC(run, dataset, disease)
